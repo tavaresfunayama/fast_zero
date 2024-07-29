ebe3916ed4fe4fd52e2369de4281cdacac45aedd
tavaresfunayama <funayamao123@gmail.com>
2024-07-29 09:11:46 -0300

    db update

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,18 +1,79 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+from fast_zero.database import get_session
+from fast_zero.models import User
 from fast_zero.schemas import Message, UserDb, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 database = []
 
 
+# @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+# def create_user(user: UserSchema):
+#     engine = create_engine(Settings().DATABASE_URL)
+
+#     with Session(engine) as session:
+#         db_user = session.scalar(
+#             select(User).where(
+#                 (User.username == user.username) | (User.email == user.email)
+#             )
+#         )
+
+#         if db_user:
+#             if db_user.username == user.username:
+#                 raise HTTPException(
+#                     status_code=HTTPStatus.BAD_REQUEST,
+#                     detail='User already exists',
+#                 )
+
+#             elif db_user.email == user.email:
+#                 raise HTTPException(
+#                     status_code=HTTPStatus.BAD_REQUEST,
+#                     detail='Email already exists',
+#                 )
+
+#         db_user = User(
+#             username=user.username, email=user.email, password=user.password
+#         )
+
+#         session.add(db_user)
+#         session.commit()
+#         session.refresh(db_user)
+
+#     return db_user
+
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDb(id=len(database) + 1, **user.model_dump())
-    database.append(user_with_id)
-    return user_with_id
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
+
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists',
+            )
+
+    db_user = User(
+        username=user.username, password=user.password, email=user.email
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get('/users/', response_model=UserList)
